e85dce06a48ad7c5f69e107c728b1cd185726fb6
adi <aditya03joshi@gmail.com>
2026-05-06 09:20:36 +0530

    redis-config

---
+++ services/location-service/internal/config/redis-config.go
@@ -0,0 +1,41 @@
+package config
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+type RedisConfig struct {
+	Host string
+	Port int
+}
+
+func LoadRedisFromEnv() RedisConfig {
+	portStr := os.Getenv("REDIS_PORT")
+	port, err := strconv.Atoi(portStr)
+	if err != nil {
+		port = 6379
+	}
+	return RedisConfig{
+		Host: os.Getenv("REDIS_HOST"),
+		Port: port,
+	}
+}
+
+func (c RedisConfig) Addr() string {
+	return fmt.Sprintf("%s:%d", c.Host, c.Port)
+}
+
+func NewRedisClient(cfg RedisConfig) *redis.Client {
+	return redis.NewClient(&redis.Options{
+		Addr:         cfg.Addr(),
+		DialTimeout:  5 * time.Second,
+		ReadTimeout:  3 * time.Second,
+		WriteTimeout: 3 * time.Second,
+		PoolSize:     10,
+	})
+}
