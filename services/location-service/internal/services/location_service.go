90d7672abad6691978d324afb98b03cb78218a8d
adi <aditya03joshi@gmail.com>
2026-05-07 16:51:53 +0530

    location service

--- services/location-service/internal/services/location_service.go
+++ services/location-service/internal/services/location_service.go
@@ -1,6 +1,11 @@
 package services
 
-import "github.com/redis/go-redis/v9"
+import (
+	"context"
+	"errors"
+
+	"github.com/redis/go-redis/v9"
+)
 
 type LocationService struct {
 	rdb *redis.Client
@@ -8,4 +13,22 @@ type LocationService struct {
 
 func NewLocationService(rdb *redis.Client) *LocationService {
 	return &LocationService{rdb: rdb}
+}
+
+const driversGeoKey = "drivers:location"
+
+func (s *LocationService) UpdateDriverLocation(
+	ctx context.Context,
+	driverID string,
+	lat float64,
+	long float64) error {
+	if driverID == "" {
+		return errors.New("driverid is required")
+	}
+
+	return s.rdb.GeoAdd(ctx, driversGeoKey, &redis.GeoLocation{
+		Name:      driverID,
+		Longitude: long,
+		Latitude:  lat,
+	}).Err()
 }
