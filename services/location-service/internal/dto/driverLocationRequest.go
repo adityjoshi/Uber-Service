6acf9f0e7b7654efb27f94dc025e8055a3837441
adi <aditya03joshi@gmail.com>
2026-05-07 16:06:51 +0530

    dto

---
+++ services/location-service/internal/dto/driverLocationRequest.go
@@ -0,0 +1,7 @@
+package dto
+
+type DriverLocationReq struct {
+	DriverID string
+	Lat      float64
+	Long     float64
+}
---
+++ services/location-service/internal/dto/nearByDriverResponse.go
@@ -0,0 +1,8 @@
+package dto
+
+type NearByDriverResponse struct {
+	DriverID string
+	Lat      float64
+	Long     float64
+	Distance float64
+}
