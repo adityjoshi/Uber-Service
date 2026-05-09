fc8299e9ea2a1eb4b1b688cdc31d01120ea6e231
adi <aditya03joshi@gmail.com>
2026-05-09 20:09:36 +0530

    location handler

--- services/location-service/internal/controller/locationController.go
+++ services/location-service/internal/controller/locationController.go
@@ -1,6 +1,10 @@
 package controller
 
 import (
+	"errors"
+	"net/http"
+
+	"github.com/adityjoshi/Uber-Service/services/location-service/internal/dto"
 	"github.com/adityjoshi/Uber-Service/services/location-service/internal/services"
 	"github.com/gin-gonic/gin"
 )
@@ -21,4 +25,25 @@ func (h *LocationHandler) RegisterRoutes(r *gin.Engine) {
 		g.GET("/driver/nearby", h.GetNearbyDriver)
 		g.DELETE("/driver/:driverID", h.RemoveDriver)
 	}
+}
+
+func (h *LocationHandler) UpdateDriverLocation(c *gin.Context) {
+	var req dto.DriverLocationReq
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid json"})
+		return
+	}
+	err := h.svc.UpdateDriverLocation(c.Request.Context(), req.DriverID, req.Lat, req.Long)
+	if err != nil {
+		if errors.Is(err, services.ErrDriverIdRequired) {
+
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "driver location updated"})
 }
