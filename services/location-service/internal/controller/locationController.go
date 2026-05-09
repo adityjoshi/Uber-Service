9291ad65c0fc6d57039f4de957057510332fa7ee
adi <aditya03joshi@gmail.com>
2026-05-09 20:21:43 +0530

    location service: Get driver location

--- services/location-service/internal/controller/locationController.go
+++ services/location-service/internal/controller/locationController.go
@@ -3,6 +3,7 @@ package controller
 import (
 	"errors"
 	"net/http"
+	"strconv"
 
 	"github.com/adityjoshi/Uber-Service/services/location-service/internal/dto"
 	"github.com/adityjoshi/Uber-Service/services/location-service/internal/services"
@@ -35,7 +36,7 @@ func (h *LocationHandler) UpdateDriverLocation(c *gin.Context) {
 	}
 	err := h.svc.UpdateDriverLocation(c.Request.Context(), req.DriverID, req.Lat, req.Long)
 	if err != nil {
-		if errors.Is(err, services.ErrDriverIdRequired) {
+		if errors.Is(err, services.ErrDriverIDRequired) {
 
 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
@@ -46,4 +47,42 @@ func (h *LocationHandler) UpdateDriverLocation(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, gin.H{"message": "driver location updated"})
+}
+
+func (h *LocationHandler) GetNearbyDriver(c *gin.Context) {
+	latStr := c.Query("latitude")
+	longStr := c.Query("longitude")
+
+	if latStr == "" || longStr == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "lat and long is required"})
+		return
+	}
+
+	latitude, err := strconv.ParseFloat(latStr, 64)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid latitude"})
+		return
+	}
+	longitude, err := strconv.ParseFloat(longStr, 64)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid longitude"})
+		return
+	}
+
+	radius := 5.0
+
+	if rs := c.Query("radius"); rs != "" {
+		radius, err := strconv.ParseFloat(rs, 64)
+		if err != nil || radius <= 0 {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "invalid radius"})
+			return
+		}
+	}
+
+	drivers, err := h.svc.FindNearbyDriver(c.Request.Context(), latitude, longitude, radius)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, drivers)
 }
