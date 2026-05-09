a62257268efa0e9f3bdb6f5766be10efcb593597
adi <aditya03joshi@gmail.com>
2026-05-09 19:53:17 +0530

    location handler

--- services/location-service/internal/controller/locationController.go
+++ services/location-service/internal/controller/locationController.go
@@ -1,6 +1,9 @@
 package controller
 
-import "github.com/adityjoshi/Uber-Service/services/location-service/internal/services"
+import (
+	"github.com/adityjoshi/Uber-Service/services/location-service/internal/services"
+	"github.com/gin-gonic/gin"
+)
 
 type LocationHandler struct {
 	svc *services.LocationService
@@ -8,4 +11,14 @@ type LocationHandler struct {
 
 func NewLocationHandler(svc *services.LocationService) *LocationHandler {
 	return &LocationHandler{svc: svc}
+}
+
+func (h *LocationHandler) RegisterRoutes(r *gin.Engine) {
+	g := r.Group("/api/v1/locations")
+
+	{
+		g.POST("/driver/update", h.UpdateDriverLocation)
+		g.GET("/driver/nearby", h.GetNearbyDriver)
+		g.DELETE("/driver/:driverID", h.RemoveDriver)
+	}
 }
