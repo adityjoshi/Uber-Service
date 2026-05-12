786d9d22786ca7e87627ebd4a6011a9447c44f4e
adi <aditya03joshi@gmail.com>
2026-05-12 14:16:50 +0530

    ride service dto

---
+++ services/ride-service/internal/dto/dto.go
@@ -0,0 +1,32 @@
+package dto
+
+import "time"
+
+type RideRequest struct {
+	RiderID       string  `json:"riderId"`
+	PickupLat     float64 `json:"pickupLat"`
+	PickupLong    float64 `json:"pickupLong"`
+	PickupAddress string  `json:"pickupAddress"`
+	DropLat       float64 `json:"dropLat"`
+	DropLong      float64 `json:"dropLong"`
+	DropAddress   string  `json:"dropAddress"`
+}
+
+type RideResponse struct {
+	Id            string     `json:"id"`
+	RiderId       float64    `json:"riderId"`
+	DriverId      float64    `json:"driverId"`
+	PickupLat     float64    `json:"pickupLat"`
+	PickupLong    float64    `json:"pickupLong"`
+	PickupAddress string     `json:"pickupAddress"`
+	DropLat       float64    `json:"dropLat"`
+	DropLong      float64    `json:"dropLong"`
+	DropAddress   string     `json:"dropAddress"`
+	Status        RideStatus `json:"status"`
+	EstimatedFare float64    `json:"estimatedFare"`
+	ActualFare    float64    `json:"actualFare"`
+	CreatedAt     time.Time  `json:"createdAt"`
+	UpdatedAt     time.Time  `json:"updatedAt"`
+	StartedAt     *time.Time `json:"startedAt"`
+	CompletedAt   *time.Time `json:"completedAt"`
+}
