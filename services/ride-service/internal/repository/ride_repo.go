d5f3ff7f4c89b8cc91c82f7759de466781d5d875
adi <aditya03joshi@gmail.com>
2026-05-13 13:32:30 +0530

    ride repo

--- services/ride-service/internal/repository/ride_repo.go
+++ services/ride-service/internal/repository/ride_repo.go
@@ -1,6 +1,12 @@
 package repository
 
-import "github.com/jackc/pgx/v5/pgxpool"
+import (
+	"context"
+	"fmt"
+
+	"github.com/adityjoshi/Uber-Service/services/ride-service/internal/model"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
 
 type RideRepository struct {
 	db *pgxpool.Pool
@@ -8,4 +14,34 @@ type RideRepository struct {
 
 func NewRideRepository(db *pgxpool.Pool) *RideRepository {
 	return &RideRepository{db: db}
+}
+
+func (r *RideRepository) Save(ctx context.Context, ride *model.Ride) error {
+	query := `
+	INSERT INTO rides (
+		id, rider_id, driver_id, pickup_latitude, pickup_longitude, pickup_address,
+		drop_latitude, drpp_longitude, drop_address, status,
+		estimated_fare, actual_fare, created_at, updated_at, started_at, completed_at
+	) VALUES (
+		$1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16
+	)
+	ON CONFLICT (id) DO UPDATE SET
+
+	driver_id = EXCLUDED.driver_id,
+	status = EXCLUDED.status,
+	estimated_fare = EXCLUDED.estimated_fare,
+	actual_fare = EXCLUDED.actual_fare,
+	started_at = EXCLUDED.started_at,
+	completed_at = EXCLUDED.completed_at
+	`
+	_, err := r.db.Exec(ctx, query, ride.ID, ride.RiderID, ride.DriverID, ride.PickupLatitude, ride.PickupLongitude, ride.PickupAddress,
+		ride.DropLatitude, ride.DropLongitude, ride.DropAddress, ride.Status, ride.EstimatedFare, ride.ActualFare, ride.CreatedAt, ride.UpdatedAt, ride.StartedAt, ride.CompletedAt,
+	)
+
+	if err != nil {
+		return fmt.Errorf("repository: save ride: %w", err)
+	}
+
+	return nil
+
 }
