a75bf1cb6d5ee5b0728992b37f28c3507eaf7b18
adi <aditya03joshi@gmail.com>
2026-05-13 10:31:01 +0530

    ride service models

--- services/ride-service/internal/dto/dto.go
+++ services/ride-service/internal/dto/dto.go
@@ -1,6 +1,10 @@
 package dto
 
-import "time"
+import (
+	"time"
+
+	"github.com/adityjoshi/Uber-Service/services/ride-service/internal/model"
+)
 
 type RideRequest struct {
 	RiderID       string  `json:"riderId"`
@@ -13,20 +17,20 @@ type RideRequest struct {
 }
 
 type RideResponse struct {
-	Id            string     `json:"id"`
-	RiderId       float64    `json:"riderId"`
-	DriverId      float64    `json:"driverId"`
-	PickupLat     float64    `json:"pickupLat"`
-	PickupLong    float64    `json:"pickupLong"`
-	PickupAddress string     `json:"pickupAddress"`
-	DropLat       float64    `json:"dropLat"`
-	DropLong      float64    `json:"dropLong"`
-	DropAddress   string     `json:"dropAddress"`
-	Status        RideStatus `json:"status"`
-	EstimatedFare float64    `json:"estimatedFare"`
-	ActualFare    float64    `json:"actualFare"`
-	CreatedAt     time.Time  `json:"createdAt"`
-	UpdatedAt     time.Time  `json:"updatedAt"`
-	StartedAt     *time.Time `json:"startedAt"`
-	CompletedAt   *time.Time `json:"completedAt"`
+	Id            string           `json:"id"`
+	RiderId       float64          `json:"riderId"`
+	DriverId      float64          `json:"driverId"`
+	PickupLat     float64          `json:"pickupLat"`
+	PickupLong    float64          `json:"pickupLong"`
+	PickupAddress string           `json:"pickupAddress"`
+	DropLat       float64          `json:"dropLat"`
+	DropLong      float64          `json:"dropLong"`
+	DropAddress   string           `json:"dropAddress"`
+	Status        model.RideStatus `json:"status"`
+	EstimatedFare float64          `json:"estimatedFare"`
+	ActualFare    float64          `json:"actualFare"`
+	CreatedAt     time.Time        `json:"createdAt"`
+	UpdatedAt     time.Time        `json:"updatedAt"`
+	StartedAt     *time.Time       `json:"startedAt"`
+	CompletedAt   *time.Time       `json:"completedAt"`
 }
