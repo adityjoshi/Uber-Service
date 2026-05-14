9f28acfad762ad9b5b86795adfe4d59d3772502d
adi <aditya03joshi@gmail.com>
2026-05-14 11:42:34 +0530

    kafka consumer

--- services/ride-service/internal/kafka/events.go
+++ services/ride-service/internal/kafka/events.go
@@ -1,5 +1,12 @@
 package kafka
 
+/*
+*
+* RideRequestedEvent is published to topic ride.requested
+* after a new ride is saved. The matching service will consume this.
+*
+* */
+
 type RideRequestedEvent struct {
 	RideID          string  `json:"rideId"`
 	RiderID         string  `json:"riderId"`
@@ -10,6 +17,12 @@ type RideRequestedEvent struct {
 	DropLongitude   float64 `json:"dropLongitude"`
 	DropAddress     string  `json:"dropAddress"`
 }
+
+/*
+*
+* RideMatchedEvent is consumed from the topic ride.matched
+* published by the matching service when a driver is assigned
+* */
 
 type RideMatchedEvent struct {
 	RideId             string  `json:"rideId"`
