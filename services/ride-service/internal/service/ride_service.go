f258e54a22f816734fa6620d81febcca3507f1ce
adi <aditya03joshi@gmail.com>
2026-05-14 12:06:22 +0530

    kafka events

---
+++ services/ride-service/internal/service/ride_service.go
@@ -0,0 +1,8 @@
+package service
+
+import "github.com/adityjoshi/Uber-Service/services/ride-service/internal/repository"
+
+type RideService struct {
+	repo     *repository.RideRepository
+	producer *kafka.Producer
+}
