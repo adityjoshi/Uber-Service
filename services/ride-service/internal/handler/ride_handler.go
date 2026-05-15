724bd77c26d8938d3a5aec6244e00eb93b03429a
adi <aditya03joshi@gmail.com>
2026-05-15 14:37:42 +0530

    ride service handler -> request ride

--- services/ride-service/internal/handler/ride_handler.go
+++ services/ride-service/internal/handler/ride_handler.go
@@ -1,6 +1,10 @@
 package handler
 
 import (
+	"log"
+	"net/http"
+
+	"github.com/adityjoshi/Uber-Service/services/ride-service/internal/dto"
 	"github.com/adityjoshi/Uber-Service/services/ride-service/internal/handler"
 	"github.com/adityjoshi/Uber-Service/services/ride-service/internal/service"
 	"github.com/gin-gonic/gin"
@@ -16,4 +20,25 @@ func NewRideHandler(svc *service.RideService) *RideHandler {
 
 func (h *RideHandler) RegisterRoutes(r gin.Engine) {
 
+}
+
+func (h *RideHandler) requestRide(c *gin.Context) {
+	var req dto.RideRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if req.RiderID == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "rider id is required"})
+		return
+	}
+	log.Printf("handler: ride request received from the ride: %s", req.RiderID)
+
+	resp, err := h.svc.RequestRide(c.Request.Context(), req)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusCreated, resp)
 }
