4185a14adaae111ae3f30a2adb893ed04f7ab0fc
adi <aditya03joshi@gmail.com>
2026-05-18 10:47:49 +0530

    kafka producer

---
+++ services/matching-service/internal/kafka/producer.go
@@ -0,0 +1,24 @@
+package kafka
+
+import (
+	"log"
+	"strings"
+
+	"github.com/segmentio/kafka-go"
+)
+
+type Producer struct {
+	writer *kafka.Writer
+}
+
+func NewProducer() *Producer {
+	brokers := strings.Split(getenv("KAFKA_BROKERS", "kafka:9092"), ",")
+
+	writer := &kafka.Writer{
+		Addr:                   kafka.TCP(brokers...),
+		Balancer:               &kafka.LeastBytes{},
+		AllowAutoTopicCreation: true,
+	}
+	log.Printf("kafka prodcuer connected to %v", brokers)
+	return &Producer{writer: writer}
+}
