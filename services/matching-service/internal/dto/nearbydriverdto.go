2a59aa662ddddc68827a093d01606669169dfc6c
adi <aditya03joshi@gmail.com>
2026-05-18 10:41:36 +0530

    dto matching service

--- services/matching-service/internal/dto/nearbydriverdto.go
+++ services/matching-service/internal/dto/nearbydriverdto.go
@@ -1,8 +1,8 @@
 package dto
 
 type NearByDriverResponse struct {
-	DriverID string
-	Lat      float64
-	Long     float64
-	Distance float64
+	DriverID string  `json:"driverId"`
+	Lat      float64 `json:"latitude"`
+	Long     float64 `json:"longitude"`
+	Distance float64 `json:"distanceInKm"`
 }
